730763d58b4151cd21f34108007c69a667eaa077
dangbinhson150801 <binhsonunity@gmail.com>
2023-11-10 16:48:43 +0700

    Lesson1

--- lesson1.js
+++ lesson1.js
@@ -1,1 +1,106 @@
-hello
+// console.log("Hello World!");
+// {
+//     console.log("Hello World!");
+//     {
+//         console.log("Hello World!");
+//         {
+//             console.log("Hello World!");
+//         }
+//     }
+// }
+// console.log("Hello World!");
+
+// {
+//     let a = 3;
+//     {
+//         console.log(a);
+//     }
+// }
+
+// console.log(a);
+
+// let a = null;
+// console.log(a);
+// let a = 2;
+
+// {
+//     var a = 7;
+// }
+// console.log(a);
+
+// var a =7;
+// var a =11;
+// console.log(a);
+
+// const PI = 3.14;
+// PI = 3;
+
+// const objA = {};
+// console.log(objA);
+// objA["name"] = "const test";
+// console.log(objA);
+
+// {
+//     x=3;
+// }
+// console.log(x);
+
+// let a = 3;
+// let b =4;
+// let c = a  ** b;
+// console.log(c);
+
+
+// let a =10;
+// let b = 10%3;
+// console.log(b);
+
+// let a=1;
+// console.log(++a);   
+// console.log(a); 
+
+// let a=1;
+// console.log(--a);   
+// console.log(a); 
+
+
+// let a = 3;
+// let b = 45;
+// a %= b;
+// console.log(a);
+
+// let a =45;
+// let b = 45;
+// console.log(a<b);
+// console.log(a>b);
+// console.log(a<=b);
+// console.log(a>=b);
+// console.log(a==b);
+
+
+// if(a =b){
+//     console.log("a");
+// }
+
+// let a = "Son";
+// let b = "test";
+// console.log(a+b);
+// a +=b;
+// console.log(a);
+
+
+// console.log(("5"===5));
+
+// let a ="undefined";
+// console.log(a);
+// if(a == undefined){
+//     console.log("aaaaa");
+// }
+
+
+let a = 5;
+console.log(a % 2 == 0 && a  < 6 && a > 0);
+console.log(a % 2 == 0 || a  < 6 || a > 0);
+console.log(a % 2 == 0 && (a  < 6 && a > 0));
+console.log(!(a>6));
+
