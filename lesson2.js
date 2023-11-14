31056a0a904cd39b958e56497e650c4da60e5262
dangbinhson150801 <binhsonunity@gmail.com>
2023-11-14 23:19:40 +0700

    Lesson2

---
+++ lesson2.js
@@ -0,0 +1,114 @@
+// console.log("Hello guys");
+// let isMale = true;
+// let a = isMale ? "Male" : "Female";
+// console.log(a);
+
+// a = undefined;
+// b = a??111;
+
+// console.log(b);
+
+// let a = 0.2;
+// let b = 0.1;
+// console.log(a+b);
+
+// console.log("20" * "10");
+
+// let b=10;
+
+// console.log(b/"abc");
+// console.log(typeof Infinity);
+
+//  let a = new Number(10);
+//  let b = new Number(10);
+//  console.log(a===b);
+// console.log(a.valueOf() == b.valueOf());
+
+//console.log(Number.MAX_VALUE);
+
+//console.log((12.46).toFixed(1));
+
+
+// console.log("abc" +"def");
+// console.log("My name is \"Son\"");
+// console.log('aaaa');
+// console.log("My name is 'Son'");
+
+//console.log("abc" == "abcd");
+
+
+// console.log("abc".length);
+
+// console.log("abc".concat("abc"));
+
+// console.log("abcdef".substring(1,3));
+
+// console.log("ABCDEF".toUpperCase());
+
+// console.log("abcdef".toLowerCase());
+
+// console.log("ABCDCEF".replace("C","HHH"));
+
+// console.log("ABCDCEF".replaceAll("C","HHH"));
+
+// {
+//     let a =2;
+//     {
+//         let a =5;
+//         for(a = 0; a <10;a++){
+//             let a =7;
+//             console.log(a);
+//         }
+//     }
+// }
+
+
+// console.log("Toi la Toi".replace(/T/i,"A"));
+// console.log("Toi la Toi".replace(/T/g,"A"));
+// console.log("Toi la Toi".replaceAll(/t/ig,"A"));
+// console.log("Toi 10 la 20 Toi".replace(/t|1/ig,"A"));
+
+// console.log("     ABc     ".trim());
+
+// let student = {
+//     name: "Son",
+//     age: 22,
+//     gender: "Male",
+//     passed:true,
+//     1:"dasdas"
+// }
+// console.log(student);
+
+// student.address = "ABC def";
+// console.log(student);
+// console.log(student.name);
+// console.log(student[1]);
+// console.log(Object.keys(student));
+// console.log(student["name"]);
+
+
+// let student = {
+//     name: "Son",
+//     age: 22,
+//     gender: "Male",
+//     passed:true,
+//     //address : "54 duong so 8",
+//     getNameAndAddress: function(){
+//        return (this.name + " "  + this.address);
+//     }
+// }
+// student.address = "ABC e3123";
+// console.log(student.getNameAndAddress());
+
+// console.log(delete student.age);
+// console.log(student);
+
+// const obj ={a:3,b:5};
+// console.log(obj);
+// obj.a = 2;
+// console.log(obj);
+
+// console.log(JSON.stringify(obj));
+
+// a= JSON.parse("{\"a\":3,\"b\":5}");
+// console.log(a);
